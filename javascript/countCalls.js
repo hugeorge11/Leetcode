317910846c25a5fb91194cd8b6457bfd685b48ec
hugeorge11 <59715018+hugeorge11@users.noreply.github.com>
2025-09-29 16:38:46 -0700

    Create countCalls.js

---
+++ javascript/countCalls.js
@@ -0,0 +1,20 @@
+// George Hu 9/28
+// countCalls.js
+// N:
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    let count = n;
+    return function() {
+        return count++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
